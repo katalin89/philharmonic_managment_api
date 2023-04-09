e6793eef53d8c8eafcf63aaaf875adbe10dd9cdf
kati1989 <kati_1989@yahoo.com>
2023-04-09 14:01:12 +0300

    first commit

---
+++ src/main/java/ro/mycode/philharmonic_managment_api/PhilharmonicManagmentApiApplication.java
@@ -0,0 +1,13 @@
+package ro.mycode.philharmonic_managment_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhilharmonicManagmentApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhilharmonicManagmentApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ro/mycode/philharmonic_managment_api/PhilharmonicManagmentApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.mycode.philharmonic_managment_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhilharmonicManagmentApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
